bd16222638bec1609810655cc070a1994104486b
Ahsen Baig <AhsenBaig@gmail.com>
2017-12-15 20:20:33 -0800

    Removed extra spaces by John.

--- src/behaviours/src/SearchController.cpp
+++ src/behaviours/src/SearchController.cpp
@@ -27,8 +27,6 @@ Result SearchController::DoWork() {
 
   Point  searchLocation;
 
-  
-
   searchLocation.x = 5.0;
   searchLocation.y = 5.0;
   //COS MOD - Result Type = WayPoint - 12/15/2017 - John Redden
